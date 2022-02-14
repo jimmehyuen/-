8a415325a3abed7b7f9a2200839228961094cdaf
Jimmy Yuen <jimmehyuen@gmail.com>
2022-02-14 13:02:59 +0000

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "ask", to: "questions#ask"
+  # get "ask" is the path (http://localhost:3000/ask)
+  # to: question (controller) #ask (method)
+  get 'ask', to: 'questions#ask'
+  get 'answer', to: 'questions#answer'
 end
